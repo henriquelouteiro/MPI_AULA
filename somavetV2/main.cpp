5c2388da29ebed97e65d4ea8fa7aa6b578261893
José Henrique A. Louteiro <78059994+henriquelouteiro@users.noreply.github.com>
2023-02-06 08:43:16 -0400

    Update main.cpp

--- somavetV2/main.cpp
+++ somavetV2/main.cpp
@@ -5,63 +5,58 @@
 
 void geraDados(int Vet[], const int tam);
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-    const int TAM = 128;
-    int rank, size, r;
-    int soma_p = 0, soma_tot = 0;
-    MPI_Status status;
+	const int TAM = 8;
+	int rank, size, r;
+	int soma_p = 0, soma_tot = 0;
+	MPI_Status status;
 
-    int Vet[TAM];
+	int Vet[TAM];
 
-    MPI_Init(&argc, &argv);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Init(&argc, &argv);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    r = TAM / size;
-    int A[r];
+	r = TAM/size;
+	int A[r];
 
-    if (rank == 0)
-        geraDados(Vet, TAM);
+	if(rank == 0)
+		geraDados(Vet, TAM);
 
-	MPI_Scatter(&Vet,r,MPI_INT, &A,r, MPI_INT,0,MPI_COMM_WORLD);
+	MPI_Scatter(&Vet, r, MPI_INT, &A, r, MPI_INT, 0, MPI_COMM_WORLD);
 
-        for (int i = 0; i < r; i++)
-        {
+	for(int i = 0; i < r; i++)
 		soma_p += A[i];
-        }
 
-	if(rank == 0){
+
+	if(rank == 0)
+	{
 		soma_tot = soma_p;
-		for(int i = 1; i<size;i++){
-			MPI_Recv(&soma_p,1,MPI_INT,i )
+		for(int i = 1; i < size; i++)
+		{
+			MPI_Recv(&soma_p, 1, MPI_INT, i, 1, MPI_COMM_WORLD, &status);
+			soma_tot += soma_p;
 		}
+
+		printf("\nSoma total: %2d.\n", soma_tot);
 	}
+	else
+		MPI_Send(&soma_p, 1, MPI_INT, 0, 1, MPI_COMM_WORLD);
+	
+	MPI_Finalize();
 
-        printf("\nSoma total: %2d.\n", soma_tot);
-    }
-    else
-    {
-        MPI_Recv(&A, r, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
-        int soma_p = 0;
-        for (int i = 0; i < r; i++)
-            soma_p += A[i];
-
-        MPI_Send(&soma_p, 1, MPI_INT, 0, 1, MPI_COMM_WORLD);
-    }
-    MPI_Finalize();
-
-    return 0;
+	return 0;
 }
 
 void geraDados(int Vet[], const int tam)
 {
-    srand(time(NULL));
+	srand(time(NULL));
 
-    printf("\nValores gerados: \n\n");
-    for (int i = 0; i < tam; i++)
-    {
-        Vet[i] = rand() % 100;
-        printf("%4d", Vet[i]);
-    }
+	printf("\nValores gerados: \n\n");
+    	for(int i = 0; i < tam; i++)
+     	{
+          	Vet[i] = rand() % 100;
+		printf("%4d", Vet[i]);
+     	}
 }
