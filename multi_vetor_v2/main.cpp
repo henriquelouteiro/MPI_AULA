023738f52789ea6adfd7adf145552aba22d11360
José Henrique A. Louteiro <78059994+henriquelouteiro@users.noreply.github.com>
2023-02-27 08:30:42 -0400

    Update main.cpp

--- multi_vetor_v2/main.cpp
+++ multi_vetor_v2/main.cpp
@@ -4,7 +4,7 @@
 #include "mpi.h"
 #include <unistd.h> // sleep(tempo em seg)
 
-#define TAMMAX 4
+#define TAMMAX 128
 
 void geraVet(int Vet[], const int tam);
 void geraMatriz(int Mat[][TAMMAX], const int tam);
@@ -14,6 +14,7 @@ int main(int argc, char* argv[])
     int rank, size, i, j;
     int matA[TAMMAX][TAMMAX], vet[TAMMAX];
     int R[TAMMAX];
+    double start, end;
 
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
@@ -30,6 +31,7 @@ int main(int argc, char* argv[])
     int matAux[tam][TAMMAX];
     int vetAux[tam];
 
+    start = MPI_Wtime();
     MPI_Bcast(&vet, TAMMAX, MPI_INT, 0, MPI_COMM_WORLD);
     MPI_Scatter(&matA, tam*TAMMAX, MPI_INT, 
                 &matAux, tam*TAMMAX, MPI_INT, 0, MPI_COMM_WORLD);
@@ -40,12 +42,14 @@ int main(int argc, char* argv[])
             vetAux[i] += matAux[i][j] * vet[j];
 
     MPI_Gather(&vetAux, tam, MPI_INT, &R, tam, MPI_INT, 0, MPI_COMM_WORLD);
-
+    end = MPI_Wtime();
     if(rank == 0)
     {
+
         printf("\nResultado:");
         for(i = 0; i < TAMMAX; i++)
             printf("\n%2d", R[i]);
+        printf("\nTempo gasto: %.8f segs", end - start);
     }
     MPI_Finalize();
     return 0;
